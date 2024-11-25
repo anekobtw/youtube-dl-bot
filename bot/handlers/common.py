290ed6d414e88a3a39d0118efa2c5039b7712fbc
anekobtw <krolexonder@gmail.com>
2024-11-25 20:06:54 +0400

    added spotify support, a few other links, and code refactorings

--- bot/handlers/common.py
+++ bot/handlers/common.py
@@ -4,7 +4,7 @@ from aiogram import Bot, F, Router, types
 from aiogram.filters import Command
 from dotenv import load_dotenv
 
-from handlers import funcs
+from handlers import downloader
 
 router = Router()
 load_dotenv()
@@ -20,7 +20,7 @@ async def start(message: types.Message) -> None:
 async def usage(message: types.Message) -> None:
     await message.answer(
         """
-<b>YouTube</b>
+<b>YouTube shorts</b>
 https://www.youtube.com/watch?v=
 https://youtu.be/
 https://www.youtube.com/shorts/
@@ -28,18 +28,23 @@ https://youtube.com/shorts/
 
 <b>Instagram</b>
 https://www.instagram.com/reel/
-
-<b>X (Twitter)</b>
-https://x.com/
-https://twitter.com/
+https://instagram.com/reel/
 
 <b>TikTok</b>
 https://www.tiktok.com/
 https://vt.tiktok.com/
 
+<b>X (Twitter)</b>
+https://x.com/
+https://twitter.com/
+
+<b>Spotify</b>
+https://open.spotify.com/track/
+
 <b>Pinterest</b>
 https://www.pinterest.com/pin/
 https://in.pinterest.com/pin/
+https://pin.it/
 """
     )
 
@@ -47,12 +52,14 @@ https://in.pinterest.com/pin/
 @router.message(F.text)
 async def message_handler(message: types.Message) -> None:
     try:
-        dl = funcs.Downloader(message.text, str(message.from_user.id))
+        dl = downloader.Downloader(message.text, str(message.from_user.id))
         filename = dl.filename
         if filename.endswith(".mp4"):
             await message.answer_video(video=types.FSInputFile(filename), caption="<b>@free_yt_dl_bot</b>")
         elif filename.endswith(".png"):
             await message.answer_photo(photo=types.FSInputFile(filename), caption="<b>@free_yt_dl_bot</b>")
+        elif filename.endswith(".mp3"):
+            await message.answer_audio(audio=types.FSInputFile(filename), caption="<b>@free_yt_dl_bot</b>")
     except Exception as e:
         await message.answer(str(e))
     else:
